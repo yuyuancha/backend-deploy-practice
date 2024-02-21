4afbdb211735c86a36da8d80ed8f414bc77fae40
sizuku <sizuku.tsai@baifu-tech.net>
2024-02-21 15:59:41 +0800

    Feat: ping api for test

---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+)
+
+func main() {
+	run()
+}
+
+// Run 啟動伺服器服務
+func run() {
+	router := gin.Default()
+	router.Use(cors.Default())
+
+	router.GET("/ping", func(context *gin.Context) {
+		context.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := router.Run(":8099")
+	if err != nil {
+		log.Fatalln("開啟 Gin 服務失敗:", err.Error())
+	}
+}
