187ac445e51e82f5bb1eb0354149c4bfa5dc04f2
sizuku <sizuku.tsai@baifu-tech.net>
2024-02-21 18:06:40 +0800

    add .env

--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("設定 ENV 發生錯誤:", err.Error())
+	}
+	fmt.Println("APP_ENV:", os.Getenv("APP_ENV"))
 	run()
 }
 
